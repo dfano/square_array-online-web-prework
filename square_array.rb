75c97609551cca0399a53220f01e4c76780e0715
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-12 18:04:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  array.each do |element|
+    new_array = []
+    new_item =  element**2
+    new_array << new_item
+    print new_array
+  end
+
 end
+array = [1,2,3]
+square_array(array)
